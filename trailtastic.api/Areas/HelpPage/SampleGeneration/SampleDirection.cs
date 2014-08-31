b3f6af99b8e60b4d9237b799aca3e6bd4c107add
Martin Fournier <martinfournier@scolab.com>
2014-08-31 12:13:26 -0400

    Base WebApi project.

---
+++ trailtastic.api/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace trailtastic.api.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
---
+++ trailtastic.api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="trailtastic.api.WebApiApplication" Language="C#" %>
