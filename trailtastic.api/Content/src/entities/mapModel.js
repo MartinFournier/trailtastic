b36897eb33b856711c5d6c0817e170c9854a00f3
Martin Fournier <martinfournier@scolab.com>
2014-08-31 15:15:53 -0400

    Browser location

--- trailtastic.api/Content/src/entities/mapModel.js
+++ trailtastic.api/Content/src/entities/mapModel.js
@@ -14,19 +14,52 @@
             zoom: 8
         },
         localStorage: new Backbone.LocalStorage("Map"),
-        
+
         initialize: function () {
             this.fetch();
             this.save();
         },
 
-        getCanvasId: function() {
+        getCanvasId: function () {
             return "#" + this.get('canvas');
         },
 
-        getCanvas: function() {
+        getCanvas: function () {
             return document.getElementById(this.get('canvas'));
         },
+
+        createMap: function () {
+            this.map = new google.maps.Map(this.getCanvas(), this.options);
+            this.initializePosition(this.map);
+        },
+
+        initializePosition: function (map) {
+            //from: http://www.rockylhc.com/blog/2012/05/17/using-gmap-requirejs-backbone
+            if (navigator && navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    function (position) {
+                        var pos = new google.maps.LatLng(
+                            position.coords.latitude,
+                            position.coords.longitude
+                        );
+                        map.setCenter(pos);
+                    }, function (e) {
+                        switch (e) {
+                            case 1:
+                                console.log('The user denied the request for location information.');
+                                break;
+                            case 2:
+                                console.log('Your location information is unavailable.');
+                                break;
+                            case 3:
+                                console.log('The request to get your location timed out.');
+                                break;
+                            default:
+                                console.log('default');
+                        }
+                    });
+            }
+        }
     });
 
     return MapModel;
--- trailtastic.api/Content/src/views/map.js
+++ trailtastic.api/Content/src/views/map.js
@@ -15,12 +15,10 @@
         },
 
         onRender: function() {
-            var height = $(window).height() - 43; // magic 43
+            var height = $(window).height() - 43;
             $(this.model.getCanvasId()).css('height', height + 'px');
 
-            var canvas = this.model.getCanvas();
-            var options = this.model.options;
-            map = new google.maps.Map(canvas, options);
+            this.model.createMap();
         }
     });
     return MapView;
